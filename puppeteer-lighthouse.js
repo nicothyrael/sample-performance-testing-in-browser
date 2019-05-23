73844bab21c4d5c393b43e8148b84292c43a632e
Nico <nico@Nicos-MacBook-Pro.local>
2019-05-23 14:33:23 -0300

    Cambios

--- puppeteer-lighthouse.js
+++ puppeteer-lighthouse.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const website = 'https://kraken-qa-37.sofitest.com';
 const puppeteer = require('puppeteer');
 const perfConfig = require('./config.performance.js');
 const fs = require('fs');
@@ -8,21 +9,27 @@ const { gatherLighthouseMetrics } = require('./helpers');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: true,
+    headless: false,
     // slowMo: 250
   });
   const page = await browser.newPage();
 
-  await page.goto('https://automationrhapsody.com/examples/sample-login/');
-  await verify(page, 'page_home');
+  await page.goto(website, {waitUntil: 'networkidle0'});
+  await verify(page, 'home_page');
 
-  await page.click('a');
-  await page.waitForSelector('form');
-  await page.type('input[name="username"]', 'admin');
-  await page.type('input[name="password"]', 'admin');
-  await page.click('input[type="submit"]');
-  await page.waitForSelector('h2');
-  await verify(page, 'page_loggedin');
+  await page.goto(website + '/b/registration?appType=pl', {waitUntil: 'networkidle0'});
+  await verify(page, 'registration_page');
+  
+  await page.type('#basicInfo_firstName', 'Nancy');
+  await page.type('#basicInfo_lastName', 'Brikhead');
+  await page.type('#reg-form > div:nth-child(4) > label', 'Pennsylvania')
+  await page.type('#email', 'testnancy+01@sofitest.com');
+  await page.type('#password1', 'password1');
+  await page.type('#password2', 'password1');
+  await page.click('#consents');
+  await page.click('#register-btn');
+  await page.waitForNavigation('networkidle2');
+  await verify(page, 'registered_user_page');
 
   await browser.close();
 })();
