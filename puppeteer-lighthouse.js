3cfa2cf80d9831439144e47de6285e550595c102
Nicolas Alonso <nalonsogb@sofi.org>
2019-05-27 18:36:57 -0300

    Updated to use data-qa (work in progress)

--- puppeteer-lighthouse.js
+++ puppeteer-lighthouse.js
@@ -1,11 +1,13 @@
 'use strict';
 
-const website = 'https://kraken-qa-37.sofitest.com';
+const website = 'https://kraken-qa-26.sofitest.com';
 const puppeteer = require('puppeteer');
 const perfConfig = require('./config.performance.js');
 const fs = require('fs');
 const resultsDir = 'results';
 const { gatherLighthouseMetrics } = require('./helpers');
+var uniqueId = Math.random().toString(36).substring(2) 
+               + (new Date()).getTime().toString(36);
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -14,29 +16,44 @@ const { gatherLighthouseMetrics } = require('./helpers');
   });
   const page = await browser.newPage();
 
-  await page.goto(website, {waitUntil: 'networkidle0'});
-  await verify(page, 'home_page');
+  //await page.goto(website, {waitUntil: 'networkidle0'});
+  //await verify(page, 'home_page');
 
   await page.goto(website + '/b/registration?appType=pl', {waitUntil: 'networkidle0'});
-  await verify(page, 'registration_page');
+  //await verify(page, 'registration_page');
   
   await page.type('#basicInfo_firstName', 'Nancy');
   await page.type('#basicInfo_lastName', 'Brikhead');
-  await page.type('#reg-form > div:nth-child(4) > label', 'Pennsylvania')
-  await page.type('#email', 'testnancy+01@sofitest.com');
+  await page.type('#reg-form > div:nth-child(4) > label', 'Pennsylvania');
+  await page.type('#email', 'nancy+'+ uniqueId +'@sofitest.com');
   await page.type('#password1', 'password1');
   await page.type('#password2', 'password1');
-  await page.click('#consents');
-  await page.click('#register-btn');
-  await page.waitForNavigation('networkidle2');
-  await verify(page, 'registered_user_page');
+  await page.click('#consents')
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('#register-btn'),
+  ]);
+  //await verify(page, 'registered_user_page');
 
-  await browser.close();
+
+  await page.type('[data-qa="amount-requested-currency-input"]', '20000');
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('[data-qa="pl-use-id-13"]')
+  ]);
+  
+  await page.type('[data-qa="date-of-birth"]', '10141963');
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click('[data-qa="citizenship-citizen"]')
+  ]);
+  
+  //await browser.close();
 })();
 
 async function verify(page, pageName) {
   await createDir(resultsDir);
-  await page.screenshot({ path: `./${resultsDir}/${pageName}.png`, fullPage: true });
+  //await page.screenshot({ path: `./${resultsDir}/${pageName}.png`, fullPage: true });
   const metrics = await gatherLighthouseMetrics(page, perfConfig);
   fs.writeFileSync(`./${resultsDir}/${pageName}.json`, JSON.stringify(metrics, null, 2));
   return metrics;
